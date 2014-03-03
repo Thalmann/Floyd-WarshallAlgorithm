087bdf2b56caf8cbb612468da4d44ff069769203
Bruno Thalmann <mr.thalmann@gmail.com>
2014-03-03 18:08:16 +0100

    added import from file and changed arraysize and converted for loops to match them

--- Floyd-WarshallAlgortihm/Floyd-WarshallAlgortihm/Program.cs
+++ Floyd-WarshallAlgortihm/Floyd-WarshallAlgortihm/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace adjacency_matrix
@@ -11,9 +12,11 @@ namespace adjacency_matrix
     class Program
     {
         static int infinity = 1000000;
+        static int tableSize = 3;
+
         static void Main(string[] args)
         {
-            int[,] adjacency_matrix = new int[100,100];
+            int[,] adjacency_matrix = new int[tableSize, tableSize];
 
             string inputFile = null;
             if (File.Exists("input.txt"))
@@ -25,30 +28,34 @@ namespace adjacency_matrix
                 Process.Start("notepad.exe", "input.txt");
             }
 
-            int i = 0;
+            inputFile = inputFile.Replace("\r\n", "").Replace(",","");
+
+            int x = 0, y = 0;
             for (int j = 0; j < inputFile.Length; j++)
             {
-                if (inputFile[j] != ',')
+                if (inputFile[j] != ';')
                 {
-                    //adjacency_matrix[i, j] = // inputFile[i].ToString();
+                    adjacency_matrix[x, y] = Convert.ToInt32(inputFile[j].ToString());
+                    y++;
                 }
                 if (inputFile[j] == ';')
                 {
-                    
+                    x++;
+                    y = 0;
                 }
             }
             
 
 
-            int[,] distance_matrix;
-            bool[,] transitive_matrix;
+            int[,] distance_matrix = new int[tableSize,tableSize];
+            bool[,] transitive_matrix = new bool[tableSize, tableSize];
 
             Console.WriteLine("Adjacency matrix:");
-            for (int x = 0; x < adjacency_matrix.Length; x++)
+            for (int z = 0; z < tableSize; z++)
             {
-                for (int y = 0; y < adjacency_matrix.Length; y++)
+                for (int c = 0; c < tableSize; c++)
                 {
-                    Console.Write(adjacency_matrix[x,y] + " ");
+                    Console.Write(adjacency_matrix[z,c] + " ");
                 }
                 Console.WriteLine("");
                 Console.WriteLine("");
@@ -57,11 +64,11 @@ namespace adjacency_matrix
             distance_matrix = floyd_Warshall(adjacency_matrix);
 
             Console.WriteLine("Distance matrix:");
-            for (int x = 0; x < distance_matrix.Length; x++)
+            for (int z = 0; z < tableSize; z++)
             {
-                for (int y = 0; y < distance_matrix.Length; y++)
+                for (int c = 0; c < tableSize; c++)
                 {
-                    Console.Write(distance_matrix[x,y] + " ");
+                    Console.Write(distance_matrix[z,c] + " ");
                 }
                 Console.WriteLine("");
                 Console.WriteLine("");
@@ -87,11 +94,11 @@ namespace adjacency_matrix
         {
             int[,] distance_matrix = adjacency_matrix;
 
-            for (int k = 0; k < adjacency_matrix.Length; k++)
+            for (int k = 0; k < tableSize; k++)
             {
-                for (int i = 0; i < adjacency_matrix.Length; i++)
+                for (int i = 0; i < tableSize; i++)
                 {
-                    for (int j = 0; j < adjacency_matrix.Length; j++)
+                    for (int j = 0; j < tableSize; j++)
                     {
                         if (distance_matrix[i,k] + distance_matrix[k,j] < adjacency_matrix[i,j])
                         {
@@ -106,10 +113,10 @@ namespace adjacency_matrix
 
         private static bool[,] transitive_Closure(int[,] adjacency_matrix)
         {
-            bool[,] transitive_matrix = new bool[1,1];
-            for (int a = 0; a < adjacency_matrix.Length; a++)
+            bool[,] transitive_matrix = new bool[tableSize, tableSize];
+            for (int a = 0; a < tableSize; a++)
             {
-                for (int b = 0; b < adjacency_matrix.Length; b++)
+                for (int b = 0; b < tableSize; b++)
                 {
                     if (adjacency_matrix[a,b] > 0 && adjacency_matrix[a,b] < infinity)
                     {
@@ -121,11 +128,11 @@ namespace adjacency_matrix
                     }
                 }
             }
-            for (int k = 0; k < adjacency_matrix.Length; k++)
+            for (int k = 0; k < tableSize; k++)
             {
-                for (int i = 0; i < adjacency_matrix.Length; i++)
+                for (int i = 0; i < tableSize; i++)
                 {
-                    for (int j = 0; j < adjacency_matrix.Length; j++)
+                    for (int j = 0; j < tableSize; j++)
                     {
                         transitive_matrix[i,j] = transitive_matrix[i,j] || (transitive_matrix[i,k] && transitive_matrix[k,j]);
                     }
